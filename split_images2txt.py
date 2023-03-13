cbf328f7e746a58eea0f0081a570cd4692d2a47f
yuqili <66876429+btobab@users.noreply.github.com>
2023-03-13 09:42:01 +0800

    Update split_images2txt.py

--- split_images2txt.py
+++ split_images2txt.py
@@ -4,7 +4,7 @@ import random
 val_percent = 0.05
 test_percent = 0.05
 train_percent = 0.9
-img_path = '../dataset/preimages'
+img_path = f'../data/preimages'
 
 total_img = os.listdir(img_path)
 num = len(total_img)
@@ -17,8 +17,8 @@ val_test_lst = [x for x in lst if x not in train_lst]
 val_lst = random.sample(val_test_lst, nv)
 test_lst = [x for x in val_test_lst if x not in val_lst]
 
-with open('../dataset/test.txt', 'w+') as ftest, open('../dataset/train.txt', 'w+') as ftrain, open(
-        '../dataset/val.txt',
+with open('../data/test.txt', 'w+') as ftest, open('../data/train.txt', 'w+') as ftrain, open(
+        '../data/val.txt',
         'w+') as fval:
     for i in lst:
         name = total_img[i][:-4] + '\n'
