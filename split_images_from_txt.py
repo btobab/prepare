7ddb7cbb5b6902ed9778081fd09fd91c00b5398c
yuqili <66876429+btobab@users.noreply.github.com>
2023-03-13 09:41:43 +0800

    Update split_images_from_txt.py

--- split_images_from_txt.py
+++ split_images_from_txt.py
@@ -4,16 +4,16 @@ import os
 modes = ["train", "val", "test"]
 data = []
 for mode in modes:
-    mode_path = f"../dataset/images/{mode}"
+    mode_path = f"../data/images/{mode}"
     if not os.path.exists(mode_path):
         os.mkdir(mode_path)
         print(f"create {mode_path}")
-    path = f"../dataset/{mode}.txt"
+    path = f"../data/{mode}.txt"
     for line in open(path, "r"):
         data.append(line.replace("\n", ""))
     for a in tqdm.tqdm(data):
         try:
-            os.system(f"cp ../dataset/preimages/{a}.jpg ../dataset/images/{mode}/{a}.jpg")
+            os.system(f"cp ../data/preimages/{a}.jpg ../data/images/{mode}/{a}.jpg")
         except:
             print(f"{a} is not exist")
     data.clear()
